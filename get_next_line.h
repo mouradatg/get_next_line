e0a23a0d2f29d99fb0f57c198a595aa6bc11cde2
Mourad Attig <mattig@student.42nice.fr>
2021-11-22 22:28:59 +0100

    Add News files

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mattig <mattig@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 19:44:05 by mattig            #+#    #+#             */
+/*   Updated: 2021/11/22 22:28:12 by mattig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_free(char *buffer, char *buf);
+char	*ft_next(char *buffer);
+char	*ft_line(char *buffer);
+char	*read_file(int fd, char *res);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+
+void	*ft_calloc(size_t count, size_t size);
+void	ft_bzero(void *s, size_t n);
+
+size_t	ft_strlen(const char *s);
+
+#endif
